ec1134704d365574894fd523691a70af98cb6122
pratik-karna <kaystha.pratik@gmail.com>
2023-04-19 08:14:38 +0530

    first commit

---
+++ multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num1, num2, res;
+    printf("Enter any two number: ");
+    scanf("%d%d", &num1, &num2);
+    res = num1*num2;
+    printf("\nMultiplication = %d", res);
+    getch();
+    return 0;
+}
